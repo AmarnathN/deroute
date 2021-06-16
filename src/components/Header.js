c0e796bf680b71c6d4e2e96ae8be3fdec7edd2c1
AmarnathN <vayuteja@gmail.com>
2021-06-16 17:48:13 +0530

    Add Main Landing text

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,19 @@
 import { AppBar, Badge, Grid, IconButton, InputBase, makeStyles, Toolbar, Typography } from "@material-ui/core";
-import { Search, Sort as SortIcon } from "@material-ui/icons";
+import { Search, Sort as SortIcon, ExpandMore as ExpandMoreIcon } from "@material-ui/icons";
 import React from "react";
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+    fontFamily: "Concert One",
+  },
+  appBar: {
     background: "none",
-    color: "white",
     fontSize: "2rem",
+    color: "white",
     fontFamily: "Concert One",
   },
   toolBar: {
@@ -16,28 +23,39 @@ const useStyles = makeStyles((theme) => ({
   title2: {
     color: "#5aFF3D",
   },
+  landingText: {
+    textAlign: "center",
+    color: "white",
+  },
 }));
 
 const Header = () => {
   const classes = useStyles();
   return (
-    <AppBar position="static" className={classes.root} elevation={0}>
-      <Toolbar className={classes.toolBar}>
-        <Grid container justify="space-between" alignItems="center">
-          <Grid item>
-            <h3>
-              De<span className={classes.title2}>Route.</span>
-            </h3>
+    <div className={classes.root}>
+      <AppBar className={classes.appBar} elevation={0}>
+        <Toolbar className={classes.toolBar}>
+          <Grid container justify="space-between" alignItems="center">
+            <Grid item>
+              <h3>
+                De<span className={classes.title2}>Route.</span>
+              </h3>
+            </Grid>
+            <Grid item sm></Grid>
+            <Grid item>
+              <IconButton color="inherit">
+                <SortIcon />
+              </IconButton>
+            </Grid>
           </Grid>
-          <Grid item sm></Grid>
-          <Grid item>
-            <IconButton color="inherit">
-              <SortIcon />
-            </IconButton>
-          </Grid>
-        </Grid>
-      </Toolbar>
-    </AppBar>
+        </Toolbar>
+      </AppBar>
+      <div className={classes.landingText}>
+        <h1>
+          Welcome to <br /> De<span className={classes.title2}>Route !</span>
+        </h1>
+      </div>
+    </div>
   );
 };
 
